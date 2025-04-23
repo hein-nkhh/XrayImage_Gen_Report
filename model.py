5beb5d6b1ba0ead5cc5a839850918db9fa0d4c6b
hoangpdh <huyhoang.190904@gmail.com>
2025-04-23 22:59:58 +0700

    Initial commit

--- model.py
+++ model.py
@@ -327,12 +327,12 @@ class EnhancedBioBARTDecoder(nn.Module):
 
         # Calculate coverage loss
         cov_out = self.coverage_weights(coverage.permute(0, 2, 1).unsqueeze(1))  # [B, 1, 1, L]
-        print("cov_out shape before squeeze:", cov_out.shape)
+        # print("cov_out shape before squeeze:", cov_out.shape)
         cov_out = cov_out.squeeze(2).permute(0, 2, 1)
-        print("cov_out shape after squeeze and permute:", cov_out.shape)
+        # print("cov_out shape after squeeze and permute:", cov_out.shape)
 
         coverage = coverage[:, :cov_out.size(1)]
-        print("coverage shape after truncating:", coverage.shape)
+        # print("coverage shape after truncating:", coverage.shape)
 
         cov_loss = self.coverage_loss_fn(cov_out, coverage)
 
