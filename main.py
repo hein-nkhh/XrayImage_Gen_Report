f3973500a7693ce9ebbf02176647c14c83172a5c
hoangpdh <huyhoang.190904@gmail.com>
2025-04-24 11:10:21 +0700

    Initial commit

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ sys.path.append('./data_utils')
 from data_utils.dataset_builder import normalize_features, to_tensor, build_dataset
 from data_utils.report_cleaning import clean_text
 sys.path.append('./training')
-from .training.train import train_step
-from .training.validate import validate
-from .training.evaluate import generate_and_decode
+from training.train import train_step
+from training.validate import validate
+from training.evaluate import generate_and_decode
 from utils.metrics_helper import calculate_bleu, calculate_meteor, calculate_rouge_scores
 from torch.utils.data import DataLoader
 from tqdm import tqdm
